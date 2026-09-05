be446b550055b7ee4f4b985a2fad38d44583abcd
Munazza588 <munazzahabib@arizona.edu>
2026-09-05 13:38:36 -0700

    display user created projects in the dashboard

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,10 +1,11 @@
 function whenClickTheNewProjectButton() {
     const projectButton = document.querySelector(".new-project-button");
     const dialogPopup = document.querySelector("#project-name-dialog")
+    const inputBox = document.querySelector("#user-project-name-input");
     projectButton.addEventListener('click',() => {
         dialogPopup.showModal();
     });
-    const inputBox = document.querySelector("#user-project-name-input");
+
     const cancelButton = document.querySelector("#cancel-button");
         cancelButton.addEventListener('click', () => {
             dialogPopup.close();
@@ -15,12 +16,18 @@ function whenClickTheNewProjectButton() {
     const submitButton = document.querySelector("#submit-button");
         submitButton.addEventListener('click', () => {
             dialogPopup.close();
+            displayTheNewProject(inputBox,projectButton);
             inputBox.value = "";
 
         });
+}
 
-
-
+function displayTheNewProject(inputBox,projectButton) {
+    const newProjectNameDisplay = document.createElement("div");
+    newProjectNameDisplay.classList.add("new-project-name");
+    newProjectNameDisplay.textContent = inputBox.value;
+    const projectDisplayDiv = document.querySelector(".dashboard");
+    projectDisplayDiv.insertBefore(newProjectNameDisplay,projectButton);
 }
 
 
